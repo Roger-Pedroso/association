aafe5f760203c8da393e87d89be4fc616c2a6642
Roger-Pedroso <94937235+Roger-Pedroso@users.noreply.github.com>
2024-04-26 22:01:48 -0300

    Refactor: ajustes dry

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    def active_status(status)
+        if status
+            content_tag(:span, class: "text-success") do
+            tag.i("", class: "bi bi-check-circle")
+            end
+        else
+            content_tag(:span, class: "text-danger") do
+            tag.i("", class: "bi bi-x-circle")
+            end
+        end
+    end
 end
